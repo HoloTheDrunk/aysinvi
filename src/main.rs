1c017666a630ad0fd056fad28baf65beb0d03d44
Raphaël Duhen <raphael.duhen.nexus@gmail.com>
2022-09-14 03:02:18 +0200

    fix(test): add trace display in case of error

--- src/main.rs
+++ src/main.rs
@@ -30,28 +30,36 @@ mod test {
 
     const TEST_FOLDER: &str = "examples/features";
 
-    fn run_test<F>(path: &str, check: F)
+    fn run_tests<F>(path: &str, check: F)
     where
         F: Fn(Result<Vec<Statement>, Trace>) -> bool,
     {
-        let folder = format!("examples/features/{path}");
-        let entries = std::fs::read_dir(folder.clone())
+        let folder = format!("{TEST_FOLDER}/{path}");
+        let mut entries = std::fs::read_dir(folder.clone())
             .unwrap_or_else(|err| panic!("{err}: Missing folder `{folder}`"));
 
-        for entry in entries {
-            let input = std::fs::read_to_string(entry.unwrap().path().to_str().unwrap()).unwrap();
+        while let Some(Ok(entry)) = entries.next() {
+            let entry = entry.path().to_str().unwrap().to_string();
+            eprintln!("Running test {entry}");
 
-            assert!(check(parse(&input)));
+            let input = std::fs::read_to_string(entry).unwrap();
+
+            let res = parse(&input);
+            if let Err(trace) = &res {
+                eprintln!("{trace}");
+            }
+
+            assert!(check(res));
         }
     }
 
     #[test]
     fn valid_expressions() {
-        run_test("expressions/valid", |output| output.is_ok());
+        run_tests("expressions/valid", |output| output.is_ok());
     }
 
     #[test]
     fn invalid_expressions() {
-        run_test("expressions/invalid", |output| output.is_err());
+        run_tests("expressions/invalid", |output| output.is_err());
     }
 }
